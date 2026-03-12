98226bfce307653a18214b0a16bca51a4b37fc93
sugu5 <sugunath5reddy@gmail.com>
2026-03-12 15:16:18 +0530

    Refactor producer service and enhance Kafka integration with topic management and metrics

--- ad_stream_producer/__init__.py
+++ ad_stream_producer/__init__.py
@@ -2,7 +2,6 @@
 This file makes the 'ad_stream_producer' directory a Python package
 and promotes key classes to the package level for easier imports.
 """
-
 from .producer_service import ProducerService
 from .schema import AdEvent
 from .kafka_producer import AdKafkaProducer
